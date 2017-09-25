cb2034739f576726c0071813e9aadc3fe5c83ca7
mbillig <marielle@billig.org>
2017-09-24 21:53:20 -0500

    added fragments for home, calendar and trends

---
+++ MoodyFoodie/app/src/main/java/com/example/marie/moodyfoodie/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.marie.moodyfoodie;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.widget.TextView;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+
+public class MainActivity extends AppCompatActivity {
+
+//    private TextView mTextMessage;
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//            return false;
+//        }
+//
+//    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        setupNavigationView();
+    //    mTextMessage = (TextView) findViewById(R.id.message);
+    //    BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    //    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private void setupNavigationView() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        if (bottomNavigationView != null) {
+            Menu menu = bottomNavigationView.getMenu();
+            selectFragment(menu.getItem(0));
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener(){
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            selectFragment(item);
+                            return false;
+                        }
+                    }
+            );
+        }
+    }
+
+    protected void selectFragment(MenuItem item) {
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                //mTextMessage.setText(R.string.title_home);
+                pushFragment(new HomeFragment());
+                break;
+            case R.id.navigation_calendar:
+                //mTextMessage.setText(R.string.title_calendar);
+                pushFragment(new CalendarFragment());
+                break;
+            case R.id.navigation_trends:
+                //mTextMessage.setText(R.string.title_trends);
+                pushFragment(new TrendsFragment());
+                break;
+        }
+    }
+
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.content, fragment);
+                ft.commit();
+            }
+        }
+    }
+
+
+}
